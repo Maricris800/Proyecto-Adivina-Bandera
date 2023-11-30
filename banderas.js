4327eb6a88647fc9664e507042e479dd68b70751
maricris800 <maricris800@hotmail.com>
2023-11-29 22:20:21 -0400

    avances del proyecto adivina la bandera

---
+++ banderas.js
@@ -0,0 +1,52 @@
+let banderas = [
+    {
+      'bandera': 'imagenes/Banderas/Argentina.jpg',
+      'opciones': ['Argentina','Uruguay','Paraguay'],
+      'opcionCorrecta': 'Argentina',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Bolivia.jpg',
+        'opciones': ['Mexico','Chile','Bolivia'],
+        'opcionCorrecta': 'Bolivia',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Brasil.jpg',
+        'opciones': ['Ecuador','Brasil','Colombia'],
+        'opcionCorrecta': 'Brasil',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Canada.jpg',
+        'opciones': ['Canada','Cuba','Panama'],
+        'opcionCorrecta': 'Canada',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Chile.jpg',
+        'opciones': ['El Salvador','Chile','Nicaragua'],
+        'opcionCorrecta': 'Chile',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Colombia.jpg',
+        'opciones': ['Guatemala','Venezuela','Colombia'],
+        'opcionCorrecta': 'Colombia',
+    },
+    {
+        'bandera': 'imagenes/Banderas/cuba.jpg',
+        'opciones': ['Cuba','Puerto Rico','España'],
+        'opcionCorrecta': 'Cuba',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Ecuador.jpg',
+        'opciones': ['Honduras','Ecuador','Bolivia'],
+        'opcionCorrecta': 'Ecuador',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Elsalvador.jpg',
+        'opciones': ['Estados Unidos','El Salvador','Uruguay'],
+        'opcionCorrecta': 'El Salvador',
+    },
+    {
+        'bandera': 'imagenes/Banderas/españa.jpg',
+        'opciones': ['Canada','Republica Dominicana','España'],
+        'opcionCorrecta': 'España',
+    },
+];
