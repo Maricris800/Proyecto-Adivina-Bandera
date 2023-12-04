ad6fb60488d3807e8b8b1b9c7e4d29a0766937f3
maricris800 <maricris800@hotmail.com>
2023-12-04 19:54:02 -0400

    mas cambios al proyecto Adivina la Bandera

--- app.js
+++ app.js
@@ -1,20 +1,60 @@
 let iniciar = document.querySelector(".boton-iniciar");
 let preguntaJuego = document.querySelector("h2");
-let bandera = document.querySelector(".contenedor-imagenes");
+let bandera = document.querySelector("img");
 let listaOpciones = document.querySelector("ul");
+let respuestaCorrecta = "Argentina";
+let contador = 0;
+ 
 
-console.log(iniciar.innerHTML);
-console.log(preguntaJuego.innerHTML);
+
+//recuperando los botones opciones de respuesta//
+let botonOpcion = document.querySelectorAll(".boton-opcion");
+let boton1 = document.querySelector("#opcion1");
+let boton2 = document.querySelector("#opcion2");
+let boton3 = document.querySelector("#opcion3");
 
 //Función para ocultar el h2 y el botón, y hacer aparecer la bandera y las opciones
 iniciar.addEventListener("click", () =>{
     cambiarEstilo();
-
+    
+    evaluar();
+    
+    
 });
 
 function cambiarEstilo(){
     iniciar.style = "display:none";
     preguntaJuego.style = "display:none";
     bandera.style = "visibility:visible";
-    listaOpciones.style = "display:flex";
+    listaOpciones.style = "display:flex";    
+}
+
+function evaluar(){  
+        
+   
+    for (let i=0; i < botonOpcion.length; i++){
+         
+       botonOpcion[i].addEventListener("click", () =>{
+            console.log(botonOpcion[i].innerText);
+            if (botonOpcion[i].innerText === respuestaCorrecta){
+                contador++;                  
+            } 
+            setTimeout(cambiarImagen,2000);
+       }); 
+              
+    }   
+}
+
+
+function cambiarImagen(){
+
+    let imagenAleatoria = Math.floor(Math.random()*11);
+
+    bandera.src = banderas[imagenAleatoria].bandera;
+    boton1.textContent = banderas[imagenAleatoria].opciones[0]; 
+    boton2.textContent = banderas[imagenAleatoria].opciones[1];
+    boton3.textContent = banderas[imagenAleatoria].opciones[2];
+
+    respuestaCorrecta = banderas[imagenAleatoria].opcionCorrecta;
+    console.log(respuestaCorrecta);
 }
--- banderas.js
+++ banderas.js
@@ -49,4 +49,54 @@ let banderas = [
         'opciones': ['Canada','Republica Dominicana','España'],
         'opcionCorrecta': 'España',
     },
+    {
+        'bandera': 'imagenes/Banderas/estUnidos.jpg',
+        'opciones': ['Estados Unidos','Puerto Rico','Uruguay'],
+        'opcionCorrecta': 'Estados Unidos',        
+    },
+    {
+        'bandera': 'imagenes/Banderas/Guatemala.jpg',
+        'opciones': ['Canada','Guatemala','Panama'],
+        'opcionCorrecta': 'Guatemala',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Honduras.jpg',
+        'opciones': ['Venezuela','Nicaragua','Honduras'],
+        'opcionCorrecta': 'Honduras',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Mexico.jpg',
+        'opciones': ['Mexico','Republica Dominicana','Ecuador'],
+        'opcionCorrecta': 'Mexico',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Nicaragua.jpg',
+        'opciones': ['El Salvador','Guatemala','Nicaragua'],
+        'opcionCorrecta': 'Nicaragua',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Panama.jpg',
+        'opciones': ['Panama','Republica Dominicana','Honduras'],
+        'opcionCorrecta': 'Panama',
+    },
+    {
+        'bandera': 'imagenes/Banderas/puertoRico.jpg',
+        'opciones': ['Canada','Puerto Rico','Ecuador'],
+        'opcionCorrecta': 'Puerto Rico',
+    },
+    {
+        'bandera': 'imagenes/Banderas/repBDeVenezuela.jpg',
+        'opciones': ['Colombia','Republica Dominicana','Venezuela'],
+        'opcionCorrecta': 'Venezuela',
+    },
+    {
+        'bandera': 'imagenes/Banderas/repDominicana.jpg',
+        'opciones': ['El Salvador','Republica Dominicana','Estados Unidos'],
+        'opcionCorrecta': 'Republica Dominicana',
+    },
+    {
+        'bandera': 'imagenes/Banderas/Uruguay.jpg',
+        'opciones': ['Uruguay','Argentina','Panama'],
+        'opcionCorrecta': 'Uruguay',
+    },
 ];
